5198f8ddb779eb29062e181d572a31575d968064
Gustavo Faquim <gustavofaquim408@gmail.com>
2023-06-06 17:40:41 -0300

    Inicio do repositório

---
+++ index.js
@@ -0,0 +1,31 @@
+
+import express from "express";
+import axios from "axios";
+
+const app = express();
+
+
+
+import IntegracaoController from "./src/controllers/IntegracaoController.js";
+
+
+// Configurando módulo ejs e a chamada das views
+app.set("view engine", "ejs");
+app.set('views', './src/views');
+app.use("/static", express.static("public"));
+
+
+// Configuração da recepção de dados pelo form
+app.use(express.urlencoded({ extended: false })); // Extrair dados do formulário e adicionar ao body
+app.use(express.json());
+
+
+
+
+// Rotas 
+app.get('/',  IntegracaoController.index)
+app.get('/api',  IntegracaoController.api)
+app.get('/auth',  IntegracaoController.autenticacao)
+
+
+app.listen(3000, () => console.log("Servidor instalado e funcionando"));
---
+++ src/controllers/IntegracaoController.js
@@ -0,0 +1,111 @@
+import axios from "axios";
+
+class IntegracaoController{
+
+    constructor() {
+        this.autenticacao = this.autenticacao.bind(this);
+        this.api = this.api.bind(this);
+    }
+ 
+    index (req,res){
+        res.render("inicio.ejs");
+    }
+
+    autenticacao(){
+
+        return new Promise((resolve, reject) => {
+
+             // const user = autenticacao.user;
+            // const pass = autenticacao.password;
+
+            const user = "gustavo.faquim";
+            const pass = "wtf@13Zy";
+            
+
+            // URL e cabeçalhos
+            const url = "https://documents.abaris.com.br/api/v1/login/";
+
+
+            const headers = {
+                'Custom-Origin': 'aee.abaris.com.br',
+                'Content-Type': 'application/json'
+            }; 
+
+
+            // Corpo da solicitação POST
+            const data = {
+                userName: 'gustavo.faquim',
+                password: 'wtf@13Zy'
+            };
+
+            // Fazer a requisição POST usando Axios
+            axios.post(url, data, { headers })
+            .then(response => {
+                //console.log(response.data);
+                const key = response.data;
+                resolve(key);
+                //res.status(200).json({success: [{msg:  key}]})
+            })
+            .catch(error => {
+                console.error(error);
+                reject(error);
+                //res.status(500).json({errors: [{msg: 'Error.'}]})
+            });
+        })
+
+    }
+
+    
+    async api(req,res){
+
+        try{
+
+            const url = 'https://documents.abaris.com.br/api/v1/document/advanced-search';
+
+            const key = await this.autenticacao()
+            //console.log(key)    
+
+            let auth = '---------------------'
+
+            if(key.rsaKey){
+                auth = key.rsaKey
+            }
+            //console.log(auth)
+
+            const headers = {
+                'x-api-key:': auth,
+                'Content-Type': 'application/json'
+            }; 
+
+           let tipoIndice = 'CPF'
+           const indice = [];
+
+           const novoElemento = {
+            nome: "Tipo de Documentos",
+            operador: "=",
+            valor: tipoIndice
+          };
+          
+          indice.push(novoElemento);
+    
+            // Fazer a requisição POST usando Axios
+            axios.post(url, {headers})
+            .then(response => {
+                //console.log(response.data);
+                console.log('Deu certo');
+                
+            })
+            .catch(error => {
+                //console.error(error);
+                res.status(500).json({errors: [{msg: 'Error.' + ' ' + error.response.data.error}]})
+            });
+
+        }catch (error) {
+            console.error('Erro na autenticação:', error);
+        }
+    }
+
+}
+
+// padrão Singleton
+export default new IntegracaoController()
