c7ae9d85aa8b285585d127e35a1ac5acb51abb7e
Gustavo Faquim <gustavofaquim408@gmail.com>
2023-06-23 16:54:55 -0300

    Implementando método do Lyceum

--- src/controllers/IntegracaoController.js
+++ src/controllers/IntegracaoController.js
@@ -90,11 +90,11 @@ class IntegracaoController{
 
             //console.log(tipoDoc)
 
-            let tipoIndice = 'FAEGO'
+            let tipoIndice = '125.43 - GRADUAÇÃO'
             const indice = [];
 
             const novoElemento = {
-                "nome": "MANTIDA",
+                "nome": "CODIGO SIGA",
                 "operador": "=",
                 "valor": tipoIndice
             };
@@ -143,11 +143,91 @@ class IntegracaoController{
         }
     }
 
+    async dadosAluno(matricula){
+
+        try{
+        
+            const url = `http://172.16.16.37:8080/api/matricula/codAluno/${matricula}/obterAluno`;
+
+            const headers = {
+                'Authorization': 'Basic YXBpdXNlcjphcGl1c2VyQDEyMw==',
+                "Content-Type": "application/json"
+            }
+
+            return new Promise((resolve, reject) => {
+                // Fazer a requisição POST usando Axios
+                axios.post(url, null, { headers })
+                .then(response => {
+                    console.log('API do Lyceum');
+                    console.log(response.data);
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    console.error(error);
+                    reject(error);
+                });
+            });
+
+        }catch(error){
+            console.error('Erro na API do Lyceum:', error);
+        }
+    }
+
     async trataDados(){
         
         try{
             const dados = await this.consultaDocumentos()
             console.log('Entrou no tratamento de dados')
+            
+            dados.forEach(e => {
+                //console.log(e)
+                //console.log('---------------');
+
+                let id_doc;
+
+                switch(e.nomeTipoDocumento){
+                    case 'CPF':
+                        id_doc = 43;
+                        break;
+                    case 'HISTÓRICO ESCOLAR':
+                        id_doc = 46;
+                        break; 
+                    case 'CERTIDÃO CASAMENTO':
+                        id_doc = 48;
+                        break;
+                    case 'CERTIDÃO NASCIMENTO':
+                        id_doc = 47;
+                        break; 
+                    case 'TÍTULO ELEITOR':
+                        id_doc = 49;
+                        break;  
+                    case 'CARTEIRA IDENTIDADE':
+                        id_doc = 44;
+                        break;   
+                    case 'TERMO RESPONSABILIDADE':
+                        id_doc = 50;
+                        break; 
+                    case 'DOCUMENTO MILITAR':
+                        id_doc = 51;
+                        break; 
+                }
+
+                let matricula = e.documentoIndice[3].valor;
+
+                const pessoa =  this.dadosAluno(matricula).pessoa;
+                
+
+                const documentosPessoais = {
+                    ID_DOCUMENTO : id_doc,
+                    PESSOA: pessoa,
+                    MATRICULA: matricula,
+
+                }
+            });
+
+            
+            
+            
             return dados
             
         }catch(error){
